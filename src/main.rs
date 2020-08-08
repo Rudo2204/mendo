3913536b0ad97ca3e941e139c3d9a03cb31c4e51
Rudo2204 <rudo2204@gmail.com>
2020-08-09 00:00:12 +0700

    Remove all the request sending code
    Request sending/handling is done in request module now

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use anyhow::{anyhow, Result};
 use clap::{App, Arg};
-use reqwest::{self, StatusCode};
-use serde::{Deserialize, Serialize};
-use serde_json::json;
 use std::io;
-//use tokio::fs;
 
 use chrono::{Local, Utc};
 use fern::colors::{Color, ColoredLevelConfig};
@@ -12,37 +8,10 @@ use log::{debug, error, info, warn, LevelFilter};
 
 mod anilist;
 mod util;
-use anilist::{oauth, query};
+use anilist::{oauth, request};
 use util::MendoConfig;
 
 const PROGRAM_NAME: &str = "mendo";
-const ANILIST_API_URL: &str = "https://graphql.anilist.co";
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct QueryResponse<T> {
-    pub data: Option<T>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct MediaTitle {
-    pub romaji: Option<String>,
-    pub english: Option<String>,
-    pub native: Option<String>,
-    pub user_preferred: Option<String>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Media {
-    pub id: Option<i32>,
-    pub title: Option<MediaTitle>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct QueryID {
-    pub media: Media,
-}
 
 fn setup_logging(verbosity: u64) -> Result<()> {
     let colors_line = ColoredLevelConfig::new()
@@ -158,70 +127,9 @@ async fn main() -> Result<()> {
         mendo_cfg = util::cfg_save_token(PROGRAM_NAME, &mut mendo_cfg, &res_token)?;
         info!("Token is saved to config file!");
     }
-    let token = mendo_cfg.token;
-    info!("Token from config file loaded. Let's get to work!");
+    info!("Token from config file is valid. Let's get to work!");
 
-    //let content = fs::read_to_string("graphql/query_user.gql").await?;
-    //let var = json!({ "id": 84994881, "progress": 109});
-    //let query = json!({ "query": content, "variables": var });
-    let query = json!({ "query": query::QUERY_USER });
-
-    let client = reqwest::Client::new();
-    debug!("Sending POST request...");
-    let res = client
-        .post(ANILIST_API_URL)
-        .header("Authorization", format!("Bearer {}", token))
-        .header("Content-Type", "application/json")
-        .header("Accept", "application/json")
-        .json(&query)
-        .send()
-        .await?;
-
-    let res_status = res.status();
-    debug!("Anilist returned code `{}'", res.status());
-
-    match res_status {
-        StatusCode::OK => {
-            println!("{}", res.text().await?);
-            //let test = res.json::<QueryResponse<QueryID>>().await?;
-            //println!("{:#?}", test);
-            info!("Anilist handled the sent request!");
-        }
-        StatusCode::TOO_MANY_REQUESTS => {
-            error!("Too many requests!");
-            return Err(anyhow!("Too many requests!"));
-        }
-        StatusCode::BAD_REQUEST => {
-            error!("400 Bad Request!");
-            return Err(anyhow!(
-                "Bad Request! Recheck your config file at {}",
-                conf_file.display()
-            ));
-        }
-        StatusCode::UNAUTHORIZED => {
-            debug!("Deleting the existing token to force user to reauth...");
-            confy::store(
-                PROGRAM_NAME,
-                MendoConfig {
-                    id: mendo_cfg.id,
-                    secret: mendo_cfg.secret,
-                    name: mendo_cfg.name,
-                    url: mendo_cfg.url,
-                    token: "Leave this field.".to_string(),
-                },
-            )?;
-            error!("Unthorized! Run the program again to reauthorize!");
-            return Err(anyhow!("Unthorized! Run the program again to reauthorize!"));
-        }
-        StatusCode::NOT_FOUND => {
-            error!("Anilist returned code `404 Not Found'!");
-            return Err(anyhow!("Anilist returned code `404 Not Found'!"));
-        }
-        _ => {
-            error!("Anilist returned an unimplemented StatusCode!");
-            return Err(anyhow!("Anilist returned an unimplemented StatusCode!"));
-        }
-    }
+    request::query_user(&mut mendo_cfg).await?;
 
     debug!("-----Everything is finished!-----");
     Ok(())
