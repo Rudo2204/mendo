046514073c30d3d9f5d9a7af6deec2f65b9dd882
Rudo2204 <rudo2204@gmail.com>
2020-08-08 23:54:39 +0700

    Add model.rs
    This module will have the structs to serde anilist's json response

---
+++ src/anilist/model.rs
@@ -0,0 +1,61 @@
+use serde::{self, Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct User {
+    pub id: i32,
+    name: String,
+    site_url: String,
+    updated_at: i64, // unix timestamp
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct MediaTitle {
+    pub romaji: Option<String>,
+    pub english: Option<String>,
+    pub native: Option<String>,
+    pub user_preferred: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Media {
+    pub title: Option<MediaTitle>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum MediaListStatus {
+    Current,
+    Planning,
+    Completed,
+    Dropped,
+    Paused,
+    Repeating,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct MediaList {
+    pub id: i32,
+    media_id: i32,
+    pub status: Option<MediaListStatus>,
+    pub progress: i32,
+    pub progress_volumes: i32,
+    pub media: Option<Media>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct MediaListGroup {
+    pub name: Option<String>,
+    pub status: Option<MediaListStatus>,
+    pub is_custom_list: Option<bool>,
+    pub entries: Option<Vec<Option<MediaList>>>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct MediaListCollection {
+    pub lists: Option<Vec<Option<MediaListGroup>>>,
+}
