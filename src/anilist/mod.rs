732a43f53852afad9418017f370d3a058bc7b52d
Rudo2204 <rudo2204@gmail.com>
2020-08-08 19:08:53 +0700

    Add CONST queries to query.rs

--- src/anilist/mod.rs
+++ src/anilist/mod.rs
@@ -1,3 +1,2 @@
+pub mod anilist;
 pub mod oauth;
-
-pub use oauth::*;
---
+++ src/anilist/query.rs
@@ -0,0 +1,49 @@
+pub const QUERY_USER: &str = "
+query {
+  Viewer {
+    id
+    name
+    siteUrl
+    updatedAt
+  }
+}
+";
+
+pub const GET_LIBRARY: &str = "
+query ($userId: Int, $type: MediaType) {
+  MediaListCollection(userId: $userId, type: $type) {
+    lists {
+      name
+      status
+      isCustomList
+      entries {
+        id
+        mediaId
+        status
+        progress
+        media {
+          title {
+            romaji
+            english
+            native
+            userPreferred
+          }
+        }
+      }
+    }
+  }
+}
+";
+
+pub const UPDATE_MEDIA: &str = "
+mutation(
+  $id: Int,
+  $mediaId: Int,
+  $progress: Int,
+) {
+  SaveMediaListEntry(id: $id, mediaId: $mediaId, progress: $progress) {
+    id
+    progress
+  }
+}
+";
